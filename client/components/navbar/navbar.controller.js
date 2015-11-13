28bc65850c8761056e5720d86cbf30116978c9a7
Mac <m@m-jo.co>
2015-11-13 17:57:18 +0100

    a start for the navbar help text thingy

--- client/components/navbar/navbar.controller.js
+++ client/components/navbar/navbar.controller.js
@@ -1,6 +1,12 @@
 'use strict';
 
 angular.module('mjocoApp')
-  .controller('NavbarCtrl', function ($scope) {
-    $scope.date = new Date();
-  });
+  .controller('NavbarCtrl', ['$rootScope', '$scope', '$location', '$log', '$window', 'localStorageService',
+    function ($rootScope, $scope, $location, $log, $window, localStorageService) {
+
+      $scope.date = new Date();
+      $scope.link = '#';
+
+      //todo: write a controller that outputs a bunch of phrases and helper text depending on the location of the browser
+      console.log(document.location.pathname);
+  }]);
